f9b581d368d5ee317d2e5656e77cef51df536e41
Ankit Gupta <ankit.mescoe@gmail.com>
2016-08-01 21:03:06 +0530

    Trying ImageIO but not of much help

--- src/main/java/com/swollenbrains/imagedescriptor/ImageDescriptor.java
+++ src/main/java/com/swollenbrains/imagedescriptor/ImageDescriptor.java
@@ -1,9 +1,52 @@
 package com.swollenbrains.imagedescriptor;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
+
 public class ImageDescriptor {
 	
 	public String getName(){
 		return "abcd";
+	}
+	
+	public String getFileName(File file) {
+		return file.getName();
+	}
+	
+	public void readMetadata(File file) throws IOException {
+		System.out.println(new File("sampleImage.jpg").getAbsolutePath());
+		BufferedImage image = ImageIO.read(new File("sampleImage.jpg"));
+		System.out.println(image!=null);
+		ImageInputStream iis = ImageIO.createImageInputStream(new File("sampleImage.jpg"));
+		System.out.println("yoho: " + iis);
+        Iterator<ImageReader> readers = ImageIO.getImageReaders(iis);
+
+        if (readers.hasNext()) {
+
+            // pick the first available ImageReader
+            ImageReader reader = readers.next();
+
+            // attach source to the reader
+            reader.setInput(iis, true);
+
+            // read metadata of first image
+            IIOMetadata metadata = reader.getImageMetadata(0);
+
+            String[] names = metadata.getMetadataFormatNames();
+            int length = names.length;
+            for (int i = 0; i < length; i++) {
+                System.out.println( "Format name: " + names[ i ] );
+                //displayMetadata(metadata.getAsTree(names[i]));
+            }
+        }
+		
 	}
 
 }
--- src/test/java/com/swollenbrains/imagedescriptor/ImageDescriptorTest.java
+++ src/test/java/com/swollenbrains/imagedescriptor/ImageDescriptorTest.java
@@ -1,15 +1,31 @@
 package com.swollenbrains.imagedescriptor;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 
 import junit.framework.Assert;
 
 public class ImageDescriptorTest {
 
+	private ImageDescriptor imageDescriptor = new ImageDescriptor();
+	
 	@Test
 	public void testGetName() {
-		ImageDescriptor imageDescriptor = new ImageDescriptor();
 		Assert.assertEquals("abcd", imageDescriptor.getName());
+	}
+	
+	@Test
+	public void testGetFileName() {
+		File sampleImage = new File("sampleImage.jpg");
+		Assert.assertEquals("sampleImage.jpg", imageDescriptor.getFileName(sampleImage));
+	}
+	
+	@Test
+	public void testReadMetadata() throws IOException {
+		File sampleImage = new File("sampleImage.jpg");
+		imageDescriptor.readMetadata(sampleImage);
 	}
 
 }
